95a9dc7c7669bf1f8f8905740f00ad9ad20144c1
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2019-06-23 08:06:17 -0400

    Properly detect windows.

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,20 +1,21 @@
 #! /usr/bin/env node
 
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const [,, ... args] = process.argv;
 let __dirname = dirname(fileURLToPath(import.meta.url));
 let shellOptions = {
-    timeout: 10
+    timeout: 10000
 };
-let isWindows = false;
+let opsys = process.platform;
+let isWindows = opsys == 'win32' || opsys == 'win64';
 let terminal = isWindows ? 'powershell.exe' : 'sh';
 let ext = isWindows ? 'ps1' : 'sh';
 let appSkeletonCmd = `${terminal} ${__dirname}/../bin/build-app-skeleton.${ext} ${args[0]} ${args[1]} ${args[2]}`;
 
-execSync(
+exec(
     appSkeletonCmd,
     shellOptions,
     (error, stdout, stderr) => {
