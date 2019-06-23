1cdb3a82fd28e3b17785c451b2c3f391d0cdb831
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2019-06-23 07:29:13 -0400

    Made jab executable.

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,20 +1,27 @@
 #! /usr/bin/env node
 
-import htmlCtrlr from './controllers/html.mjs';
-import express from 'express';
-import settings from '../config/settings.mjs';
+import { execSync } from 'child_process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express();
 const [,, ... args] = process.argv;
+let __dirname = dirname(fileURLToPath(import.meta.url));
+let shellOptions = {
+    timeout: 10
+};
+let isWindows = false;
+let terminal = isWindows ? 'powershell.exe' : 'sh';
+let ext = isWindows ? 'ps1' : 'sh';
+let appSkeletonCmd = `${terminal} ${__dirname}/../bin/build-app-skeleton.${ext} ${args[0]} ${args[1]} ${args[2]}`;
 
-app.route('/')
-    .get((req, res) => {
-        let responseStr = htmlCtrlr({
-            html: 'Hello World',
-            title: 'JAB'
-        });
-
-        res.send(responseStr);
-    });
-
-app.listen(settings.port);
+execSync(
+    appSkeletonCmd,
+    shellOptions,
+    (error, stdout, stderr) => {
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+            return;
+        }
+        console.log(stdout, "\n", stderr);
+    }
+);
